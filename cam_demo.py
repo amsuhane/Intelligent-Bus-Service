20925c1da8adefc3daba2f3651f0aadb01cddc50
Shreyas <shreyaskowshik@gmail.com>
2019-04-13 00:16:36 +0530

    Updated

--- cam_demo.py
+++ cam_demo.py
@@ -4,7 +4,7 @@ import cv2
 from backend.det import PersonDetector
 
 print("Loading model...")
-# clf = PersonDetector()
+clf = PersonDetector()
 print("Model loaded")
 
 img = None
@@ -25,20 +25,21 @@ while(True):
 
 print("Received Image")
 
+IMG_W = img.shape[1]
+IMG_H = img.shape[0]
+boxes,scores,classes,num = clf.get_classification(img)
+
+persons = 0
+for i in range(boxes.shape[1]):
+    if scores[0][i] < 0.5 or classes[0][i] != 1:
+        continue
+    persons+=1
+    best_box = boxes[0][i]
+    cv2.rectangle(img,(int(best_box[1]*IMG_W),int(best_box[0]*IMG_H)),(int(best_box[3]*IMG_W),int(best_box[2]*IMG_H)),(0,255,0),3)
+
+print("---------------------------")
+print("{} Persons Detected".format(persons))
+
 cv2.imwrite("cam.jpg",img)
-# IMG_W = img.shape[1]
-# IMG_H = img.shape[0]
-# boxes,scores,classes,num = clf.get_classification(img)
-
-# persons = 0
-# for i in range(boxes.shape[1]):
-#     if scores[0][i] < 0.5 or classes[0][i] != 1:
-#         continue
-#     persons+=1
-#     best_box = boxes[0][i]
-#     cv2.rectangle(img,(int(best_box[1]*IMG_W),int(best_box[0]*IMG_H)),(int(best_box[3]*IMG_W),int(best_box[2]*IMG_H)),(0,255,0),3)
-
-# print("---------------------------")
-# print("{} Persons Detected".format(persons))
-# cv2.imshow("IMG",img)
-# cv2.waitKey(0)
+cv2.imshow("IMG",img)
+cv2.waitKey(0)
